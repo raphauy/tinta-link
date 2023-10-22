c8a11193d283b6996e334f2ed01fc8672e29a019
raphauy <rapha.uy@rapha.uy>
2023-10-22 08:54:07 -0300

    first commit

--- src/app/user/link-box.tsx
+++ src/app/user/link-box.tsx
@@ -22,7 +22,7 @@ export default function LinkBox({ href }: Props) {
     return (
         <div className="flex items-center border gap-4 py-2 rounded-full px-10 mt-1 shadow-lg dark:shadow-white">
             <Link href={href} target="_blank">
-                    <Button variant="link" className="px-1">{href}</Button>
+                    <Button variant="link" className="px-1">{href.split("//")[1]}</Button>
             </Link>
             <Button variant="ghost" onClick={copyLinkToClipboard} className="px-2">
                 <Copy size={20} />
