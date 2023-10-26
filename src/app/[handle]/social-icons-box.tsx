f114ff245078a7b6fe53986062d530fec76a28fd
raphauy <rapha.uy@rapha.uy>
2023-10-26 08:11:44 -0300

    aria-labels

--- src/app/[handle]/social-icons-box.tsx
+++ src/app/[handle]/social-icons-box.tsx
@@ -12,7 +12,10 @@ export default function SocialIconsBox({ title, href, icon, color }: Props) {
     const socialIcon= getReactIcon(icon)
 
     return (
-        <Link href={href} target="_blank" rel="noopener noreferrer"
+        <Link href={href} 
+            target="_blank" 
+            rel="noopener noreferrer"
+            aria-label="Social link"
             className="flex items-center p-1 hover:scale-110  transition-all ">
             <div className="flex text-center">
                 <div>
